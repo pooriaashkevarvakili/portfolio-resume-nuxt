9069613b169b6e498635cfe1f9fd50d47833a76d
pooriavakili09 <pooriavakili09@gmail.com>
2023-06-03 15:26:03 +0330

    updatecode

---
+++ components/blogTwo.vue
@@ -0,0 +1,85 @@
+<template>
+    <div
+        class="w-full   4xl:ml-6  2xl:w-1/2 xl:w-1/2 lg:w-1/2  4xl:w-11/12 sm:w-full md:w-full  5xl:w-full 2xl:ml-0 xl:ml-0 lg:ml-0 md:-ml-4 sm:ml-0 ">
+        <div class="my-4 md:mx-4 shadow p-6 rounded-md dark:bg-slate-800 bg-white group hover:shadow-md">
+            <div class="relative mb-6 w-full h-48 bg-purple-200 rounded-md overflow-hidden">
+                <img src="../public/logoTwo.jpg" alt="coverImage" class="w-full h-full object-cover object-center" />
+                <div
+                    class="absolute opacity-0 transition duration-200 bg-purple-100 bg-opacity-80 w-full flex items-center justify-center h-full left-0 top-0 group-hover:opacity-100 group-hover:visible">
+                    <ul class="flex flex-wrap">
+                        <li class="m-2">
+                            <a href="https://virgool.io/@pooriavakili09/%D8%A8%DB%8C%D9%88%DA%AF%D8%B1%D8%A7%D9%81%DB%8C-%D9%82%D8%B3%D9%85%D8%AA-%D8%AF%D9%88%D9%85-cjt2kij48tq2"
+                                target="_blank" rel="noreferrer"
+                                class="w-12 h-10 text-xl text-purple-600 flex items-center justify-center border-2 border-purple-600 rounded hover:text-white hover:bg-purple-600">
+                                <eye />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <h3 dir="auto" class="text-lg font-medium dark:text-white text-gray-800 mb-2">{{ $t('biographipartwo', {
+                name: 'biographipartwo'
+            })
+            }}
+            </h3>
+        </div>
+    </div>
+    <div
+        class="w-full   4xl:ml-6  2xl:w-1/2 xl:w-1/2 lg:w-1/2  4xl:w-11/12 sm:w-full md:w-full  5xl:w-full 2xl:ml-0 xl:ml-0 lg:ml-0 md:-ml-4 sm:ml-0 ">
+        <div class="my-4 md:mx-4 shadow p-6 rounded-md dark:bg-slate-800 bg-white group hover:shadow-md">
+            <div class="relative mb-6 w-full h-48 bg-purple-200 rounded-md overflow-hidden">
+                <img src="../public/logoTwo.jpg" alt="coverImage" class="w-full h-full object-cover object-center" />
+                <div
+                    class="absolute opacity-0 transition duration-200 bg-purple-100 bg-opacity-80 w-full flex items-center justify-center h-full left-0 top-0 group-hover:opacity-100 group-hover:visible">
+                    <ul class="flex flex-wrap">
+                        <li class="m-2">
+                            <a href="https://virgool.io/@pooriavakili09/%D8%A8%DB%8C%D9%88%DA%AF%D8%B1%D8%A7%D9%81%DB%8C-%D9%82%D8%B3%D9%85%D8%AA-%D8%B3%D9%88%D9%85-hael9lqgqwvs"
+                                target="_blank" rel="noreferrer"
+                                class="w-12 h-10 text-xl text-purple-600 flex items-center justify-center border-2 border-purple-600 rounded hover:text-white hover:bg-purple-600">
+                                <eye />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <h3 dir="auto" class="text-lg font-medium dark:text-white text-gray-800 mb-2">{{ $t('biographiparthree', {
+                name: 'biographiparthree'
+            })
+            }}
+            </h3>
+        </div>
+    </div>
+    <div
+        class="w-full   4xl:ml-6  2xl:w-1/2 xl:w-1/2 lg:w-1/2  4xl:w-11/12 sm:w-full md:w-full  5xl:w-full 2xl:ml-0 xl:ml-0 lg:ml-0 md:-ml-4 sm:ml-0 ">
+        <div class="my-4 md:mx-4 shadow p-6 rounded-md dark:bg-slate-800 bg-white group hover:shadow-md">
+            <div class="relative mb-6 w-full h-48 bg-purple-200 rounded-md overflow-hidden">
+                <img src="../public/logoTwo.jpg" alt="coverImage" class="w-full h-full object-cover object-center" />
+                <div
+                    class="absolute opacity-0 transition duration-200 bg-purple-100 bg-opacity-80 w-full flex items-center justify-center h-full left-0 top-0 group-hover:opacity-100 group-hover:visible">
+                    <ul class="flex flex-wrap">
+                        <li class="m-2">
+                            <a href="https://virgool.io/@pooriavakili09/%D8%A8%DB%8C%D9%88%DA%AF%D8%B1%D8%A7%D9%81%DB%8C-%D9%BE%D9%88%D8%B1%DB%8C%D8%A7-%D8%A7%D8%B4%DA%A9%D9%88%D8%B1%D9%88%DA%A9%DB%8C%D9%84%DB%8C-lzsjm017vdpu"
+                                target="_blank" rel="noreferrer"
+                                class="w-12 h-10 text-xl text-purple-600 flex items-center justify-center border-2 border-purple-600 rounded hover:text-white hover:bg-purple-600">
+                                <eye />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <h3 dir="auto" class="text-lg font-medium dark:text-white text-gray-800 mb-2">{{ $t('biographipartFour', {
+                name: 'biographipartFour'
+            })
+            }}
+            </h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
